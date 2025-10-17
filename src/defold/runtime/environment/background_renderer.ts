761c3882d6c8b268132e7a62405d281a667031d2
roblesisaac <irobles1030@gmail.com>
2025-10-17 13:17:49 -0700

    Render well in background grid

--- src/defold/runtime/environment/background_renderer.ts
+++ src/defold/runtime/environment/background_renderer.ts
@@ -1,5 +1,6 @@
 import { drawGrass } from '../../gui/drawGrass.js';
 import { drawTree } from '../../gui/drawTree.js';
+import { drawWell } from '../../gui/drawWell.js';
 import { ctx, canvasWidth, groundY } from '../state/rendering_state.js';
 
 export function drawBackgroundGrid(cameraY: number): void {
@@ -17,5 +18,12 @@ export function drawBackgroundGrid(cameraY: number): void {
     foliageDensity: 0.78,
     bushiness: 1.25,
     seed: 0x22b
+  });
+
+  drawWell(ctx, {
+    x: canvasWidth * 0.7,
+    groundY,
+    cameraY,
+    scale: 0.85
   });
 }
