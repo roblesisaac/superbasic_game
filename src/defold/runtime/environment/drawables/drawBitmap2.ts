b4ff339934a5833e467e6eb735ed4696bac64965
irobles1030 <irobles1030@gmail.com>
2025-11-06 01:40:52 +0000

    debug: pixel appear effect

--- src/defold/runtime/environment/drawables/drawBitmap2.ts
+++ src/defold/runtime/environment/drawables/drawBitmap2.ts
@@ -259,7 +259,7 @@ export function drawBitmap2(
     darken = 0,
     transparentChars = " ",
 
-    randomAppear = false,
+    randomAppear = true,
     pixelsPerFrame = 64,
     resetRandomAppear = false,
   } = config;
